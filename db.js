1da62730b4518d5e51194845be6922cf3909cb82
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-25 21:42:54 -0800

    Add DATABASE_URL  for Heroku deploy

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
-
 const { Pool } = require("pg");
 
-const dbConfig = {
-  user: process.env.DB_USER || "vagrant",
-  password: process.env.DB_PASS || "123",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "loan-payment",
-};
+let dbConfig = {};
+if (process.env.DATABASE_URL) {
+  dbConfig.connectionString = process.env.DATABASE_URL;
+} else {
+  dbConfig = {
+    user: process.env.DB_USER || "vagrant",
+    password: process.env.DB_PASS || "123",
+    host: process.env.DB_HOST || "localhost",
+    database: process.env.DB_NAME || "loan-payment",
+    port: process.env.DB_PORT || 5432,
+  };
+}
 
 console.log(dbConfig);
 const pool = new Pool(dbConfig);
